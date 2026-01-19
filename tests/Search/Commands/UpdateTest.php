2279d3e06df1620a910073bf17a8427f611c4b33
Michael Stokoe <mstokoe0990@gmail.com>
2026-01-19 09:49:53 +0000

    Added placeholder tests for chunked search update feature

---
+++ tests/Search/Commands/UpdateTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Search\Commands;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class UpdateTest extends TestCase
+{
+    #[Test]
+    public function it_updates_index_normally_without_chunk_option()
+    {
+        $this->markTestIncomplete(); // todo
+    }
+
+    #[Test]
+    public function it_updates_index_in_chunks_when_chunk_option_is_provided()
+    {
+        $this->markTestIncomplete(); // todo
+    }
+
+    #[Test]
+    public function it_does_not_call_update_method_when_using_chunk_option()
+    {
+        $this->markTestIncomplete(); // todo
+    }
+
+    #[Test]
+    public function it_outputs_progress_during_chunked_update()
+    {
+        $this->markTestIncomplete(); // todo
+    }
+
+    #[Test]
+    public function chunk_option_processes_correct_number_of_items()
+    {
+        $this->markTestIncomplete(); // todo
+    }
+}
